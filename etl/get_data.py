19af473adf57cde95a6c7fd99a598d686d718b02
larry6point6 <larry.adeyemi@6point6.co.uk>
2022-01-20 16:33:49 +0000

    restructure project and start the extracting of logic for the etl once now is complete, more work needs to be done on the transformations/cleaning

---
+++ etl/get_data.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+
+# Structure of URL is base + name_of_league + season_start
+
+base = "https://understat.com/"
+
+# Initially will just use Premier League but
+# this list gives us flexibility if we want to look at other leagues
+# or later pull all the data
+leagues = ["EPL", "La Liga", "Bundesliga", "Serie A", "Ligue 1", "RPFL"]
+seasons = ["2014", "2015", "2016", "2017", "2018", "2019", "2021"]
+
+# To select another league/season modify the below line accordingly
+url = f"{base}/{leagues[0]}/{seasons[-1]}"
+
+
+def download_data(base=url):
+    r = requests.get(url)
+    print(r.raise_for_status)
+
+    return r
+
+
+def create_soup(data):
+    soup = BeautifulSoup(
+        data.content,
+        "lxml",
+    )
+    # We know that we're looking for the script tag as this houses our data
+    scripts = soup.find_all("script")
+
+    return scripts
